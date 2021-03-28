445fb48c8b93aa941a96d844a940debcbfd797d0
SBkim12 <mar5924@naver.com>
2021-03-28 21:41:45 +0900

    21/03/28 실패율

--- level1(java)/src/실패율.java
+++ level1(java)/src/실패율.java
@@ -1,53 +1,42 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Stream;
-
 public class 실패율 {
 
 	public static void main(String[] args) {
 		int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
 		int N = 5;
-		
-		int[] pass = new int[5];
-		int[] nope = new int[5];
-		for(int i=0; i<stages.length; i++) {
-			for(int j=0; j<N; j++) {
-				if(j==stages[i]-1) {
-					nope[j]++;
-				}
-				if(j<=stages[i]-1) {
-					pass[j]++;
-				}
-				
-			}
-		}
-		
-		Map<Integer, Double> rMap = new HashMap<>();
-		for(int i=0; i<N; i++) {
-			rMap.put(i+1, nope[i]/(double)pass[i]);
-		}
-		
-		int[] count = new int[N];
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				if(i==j) {
-					continue;
-				}else {
-					if(rMap.get(i)>=rMap.get(j)) {
-						count[i]++;
-					}
-				}
-				
-			}
-			
-		}
-		
-		// 어려웡
-		
-//		for(int i=0; i<N; i++) {
-//			System.out.println(i +  " : " +pass[i]);
-//			System.out.println(i +  " : " +nope[i]);
-//		}
+
+		int[] answer = new int[N];
+		double[] failureRates = new double[N];
+		for (int i = 1; i <= N; i++) {
+            int stage = i;
+            double failPerson = 0; // 도달만 한자
+            double passPerson = 0; // 넘어간 자
+            for (int j = 0; j < stages.length; j++) {
+                if (stage <= stages[j]) {              
+                    passPerson++;
+                }
+                if (stage == stages[j]) {            
+                }
+            }
+            if(failPerson == 0) {                       
+                failureRates[i -1] = 0; 
+                continue;
+            }
+            failureRates[i - 1] = failPerson / passPerson; 
+        }
+
+		for (int i = 0; i < failureRates.length; i++) {
+            double max = -1;                             
+            int maxIdx = 0;
+            for (int j = 0; j < failureRates.length; j++) {
+                if (max < failureRates[j]) {              
+                    max = failureRates[j];
+                    maxIdx = j;
+                }
+            }
+            failureRates[maxIdx] = -1;                      
+            answer[i] = maxIdx+1;
+            System.out.println(answer[i]);
+        }
 
 	}
 
