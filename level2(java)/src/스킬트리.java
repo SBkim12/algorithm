d798e2baf07734219e99f50053ce92106c707ddf
SBkim12 <mar5924@naver.com>
2021-04-02 23:57:33 +0900

    21/04/02 스킬트리 푸는중

---
+++ level2(java)/src/스킬트리.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class 스킬트리 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String skill = "ACHQ";
+		String[] skill_trees = {"ABC", "CA", "ATEW", "SFCQTA"};
+		System.out.println(solution(skill, skill_trees));
+	}
+
+	public static int solution(String skill, String[] skill_trees) {
+		int answer = 0;
+		
+		
+		for (int i = 0; i < skill_trees.length; i++) {
+			List<Integer> index = new ArrayList<>();
+			for(int j=0; j<skill_trees[i].length(); j++) {
+				for(int e =0; e<skill.length(); e++) {
+					if(skill_trees[i].charAt(j)==skill.charAt(e)) {
+						index.add(e);
+						break;
+					}
+				}
+			}
+			
+			boolean possible = true;
+			for(int j=0; j<index.size()-1; j++) {
+				if(index.get(j)==index.get(j+1)-1) {
+					possible = true;
+				}else {
+					possible = false;
+					break;
+				}
+				if(index.get(0)!=0) {
+					possible = false;
+					break;
+				}
+			}
+			if(possible == true) {
+				answer++;
+			}
+			
+		}
+
+		return answer;
+	}
+}
