cde95cfaa199945152a576fb30e7180e87d8ee55
SBkim12 <mar5924@naver.com>
2021-06-23 23:20:13 +0900

    21/06/23 방문길이

---
+++ level2(java)/src/방문길이.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+
+public class 방문길이 {
+
+	public static int solution(String dirs) {
+        int answer = 0;     
+        int x = 0, y = 0;
+        
+        HashSet<String> set = new HashSet<>();
+        
+        for(Character dir : dirs.toCharArray()){
+            int lastX = x, lastY = y;
+            
+            if(dir == 'U' && y+1 <= 5) y++;
+            if(dir == 'D' && y-1 >= -5) y--;
+            if(dir == 'R' && x+1 <= 5) x++;
+            if(dir == 'L' && x-1 >= -5) x--;   
+            
+            if(lastX == x && lastY == y){
+                continue;
+            }
+            
+            //양방향 다 넣어주기
+            set.add(lastX+""+lastY+""+x+""+y);
+            set.add(x+""+y+""+lastX+""+lastY);
+        }
+        
+        answer = set.size()/2;
+        return answer;
+    }
+	
+}
